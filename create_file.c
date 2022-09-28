cdcb5100e6cb731caa7350539f4a732b0004834e
Amine-bc <lm_bouchoucha@esi.dz>
2022-09-28 22:46:11 +0100

    first commit

---
+++ create_file.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+//ask the user for the name of the file he wants to create 
+char file_name[256];
+char file_full[266];
+strcpy(file_full,"./files/");
+printf("What is the name of the file you want to process ? \n PS: add the extention please \n");
+scanf("%s",&file_name);
+FILE *f ;
+f = fopen(file_name,"w");
+strcat(file_full,file_name);
+printf("What do you want to write ? / PS: max size is 255 characters \n");
+char string_s[255];
+char *temp;
+scanf("%c",&temp); // temp statement to clear buffer
+fgets(string_s,30,stdin);
+printf("%s \n",string_s);
+fprintf(f,"%s",string_s);
+
+
+
+
+return 0;
+};
