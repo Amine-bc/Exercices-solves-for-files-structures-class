b23528ef32bf63ea880d2503dc9e3f0fadd77358
Amine-bc <lm_bouchoucha@esi.dz>
2022-10-13 09:05:59 +0100

    Exercice 3

--- exos/ex3/3.c
+++ exos/ex3/3.c
@@ -3,23 +3,29 @@
 #include <string.h>
 #include <ctype.h>
 int main() {
-    char mot[26], motf[26], c,cmp;
-    int stop,i =0;
-    file *f = fopen("3.txt","r") ;
+    char mot[26], motf[26], c;
+    int stop=0,i=0,cmp =0;
+    motf[0] ="";
+    FILE *f = fopen("3.txt","r") ;
+    printf("Hello \n");
     scanf("%s",&mot);
-    while ( (c=fgetc(f)) != EOF )
+    printf("%s",mot);
+    printf("----------file----------\n");
+    while ((c=fgetc(f))!= EOF) || (stop==
     {
         if (isalpha(c)){
             motf[i]=c;
             i++;
+            printf("%s, i = %d \n",motf,i);
         }else
         {
             if (i!=0)
             {
                 motf[i] = '\0' ;
-                if(strcmp(mot, motf))
+                if(strcmp(mot, motf)==0)
                 {
                     cmp ++;
+                    printf("cmp = %d \n",cmp);
                 }
                 i = 0;
             }
@@ -28,5 +34,6 @@ int main() {
         }
 
     }
+    printf("\n ------------------- \n Nombre d'occurence: %i \n",cmp);
 
 }
